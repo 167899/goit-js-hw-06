df18ab0ff58456e96bb6063f9e27280316014f92
Antonenko <42521426+167899@users.noreply.github.com>
2022-04-17 15:17:47 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listElements = document.getElementById("ingredients");
+
+// for (const element of ingredients) {
+//   const ingredient = document.createElement('li');
+//   ingredient.textContent = element;
+//   ingredient.classList.add('item');
+//   listElements.append(ingredient);
+// }
+
+  ingredients.map((el) => {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = el;
+  ingredient.classList.add('item');
+  listElements.append(ingredient);
+  return ingredient;
+})
