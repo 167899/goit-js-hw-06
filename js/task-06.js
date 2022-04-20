71b601f9113c43be4e1ecb4ca405b8f38fa98e5d
Antonenko <42521426+167899@users.noreply.github.com>
2022-04-20 17:13:54 +0300

    task-06-fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,7 @@
 const input = document.querySelector('#validation-input');
-// const inputLength = document.querySelector('[data-length="6"]');
-
-console.log(input);
 
 const validationInput = () => {
+
   if (input.value.length === Number(input.dataset.length)) {
     input.classList.add('valid');
     input.classList.remove('invalid');
@@ -11,6 +9,7 @@ const validationInput = () => {
     input.classList.add('invalid');
     input.classList.remove('valid');
   }
+  
 }
 
 input.addEventListener('blur', validationInput);
