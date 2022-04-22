3dd4b22a14931ea21654663fda733b5ff70907e5
Antonenko <42521426+167899@users.noreply.github.com>
2022-04-22 19:28:42 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector("form.login-form");
 
+const onSubmit = (event) => {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value &&
+    event.currentTarget.elements.password.value !== ""
+  ) {
+    // const email = event.currentTarget.elements.email.value;
+    // const password = event.currentTarget.elements.password.value;
+
+    // const objData = { email, password };
+    // console.log(objData);
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    formData.forEach((a, b) => {
+      console.log(b, a);
+    });
+  } else {
+    alert("Bсе поля должны быть заполнены!");
+  }
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", onSubmit);
